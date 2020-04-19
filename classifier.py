9fc12b7b9b4bf4e7dcec37daee006cbeca916b77
Chitrasen Mohanty <mohantychitrasen@gmail.com>
2020-04-19 02:14:50 -0500

    added data_cleaning and added experiment2 in classifier

--- classifier.py
+++ classifier.py
@@ -9,12 +9,12 @@ import numpy as np
 import pandas as pd
 import os
 
-from sklearn.ensemble import RandomForestClassifier
-#from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+# from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
@@ -22,81 +22,94 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.metrics import f1_score, make_scorer, precision_score, recall_score, accuracy_score
 from sklearn.metrics import average_precision_score
 
-#from sklearn.metrics import precision_recall_curve
+# from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import plot_precision_recall_curve
 import matplotlib.pyplot as plt
 
 import sklearn
-#from sklearn.inspection import permutation_importance
+# from sklearn.inspection import permutation_importance
 from sklearn.utils import resample
 
+from labeler import read_human_labels
 random_state = 100
 scorer = make_scorer(f1_score)
-#preprocess_params = {}
+# preprocess_params = {}
 
 models = {'RandomForestClassifier': RandomForestClassifier(),
-         'LogisticRegression': LogisticRegression(),
-         #'SVC' : SVC()
-         }
+          'LogisticRegression': LogisticRegression(),
+          #'GradientBoostingClassifier': GradientBoostingClassifier(),
+          # 'SVC' : SVC()
+          }
 
-model_params = {'RandomForestClassifier':
-              {'n_estimators':[10],
-               'max_depth': [None, 15, 20, 25, 30],
-              'min_samples_split' : [2, 3],
-              'min_samples_leaf': [1, 3, 5, 7],
-              'min_weight_fraction_leaf': [0],
-              'max_features': ['auto'],
-              'n_jobs': [-1],
-              'random_state':[random_state],
-              'class_weight':['balanced', None]
-              },
-            'LogisticRegression':{
-                'penalty':['l1','l2'],
-                'C':[1.e-1, 1, 1.e1]
-            },
-            'SVC':
-              {'C':[1.e-05, 1.e-03, 1.e-01],
-              'kernel':['rbf', 'linear'],
-              'gamma' : ['auto', 1.e-01],
-              'shrinking':[True, False],
-              'class_weight':['balanced'],
-              'random_state':[random_state]},
-         }
+model_params = {'RandomForestClassifier': {
+    'n_estimators': [100],
+    'max_depth': [None, 12, 15, 17],
+    'min_samples_split': [2, 3],
+    'min_samples_leaf': [5, 7, 9],
+    'min_weight_fraction_leaf': [0],
+    'max_features': ['auto'],
+    'n_jobs': [-1],
+    'random_state': [random_state],
+    'class_weight': ['balanced', None]
+},
+    'LogisticRegression': {
+        'penalty': ['l1', 'l2'],
+        'C': [1.e-1, 1, 1.e1, 1e2, 1e3]
+    },
+    'GradientBoostingClassifier': {
+        'loss': ['deviance', 'exponential'],
+        'learning_rate': [0.1],
+        'n_estimators': [100],
+        'min_samples_leaf': [1, 3, 5],
+        'max_depth': [3, 4],
+        'random_state': [random_state]
+    },
+    'SVC':
+        {'C': [1.e-05, 1.e-03, 1.e-01],
+         'kernel': ['rbf', 'linear'],
+         'gamma': ['auto', 1.e-01],
+         'shrinking': [True, False],
+         'class_weight': ['balanced'],
+         'random_state': [random_state]},
+}
 
-#loading data
+
+# loading data
 def load_train():
-    DATA_FILE_TRAIN = '../data/label_model_output/df_train_filtered.csv' #NB changed since not in notebooks subdirectory
+    DATA_FILE_TRAIN = '../data/label_model_output/df_train_filtered.csv'  # NB changed since not in notebooks subdirectory
     df = pd.read_csv(DATA_FILE_TRAIN)
     comments = df.Cleaned_Text.values
     y = np.round(df.prob)
-    
+
     return comments, y
 
+
 def load_test():
-    DATA_FILE_TEST = '../data/label_model_output/df_test.csv' #NB changed since not in notebooks subdirectory
+    DATA_FILE_TEST = '../data/label_model_output/df_test.csv'  # NB changed since not in notebooks subdirectory
     df = pd.read_csv(DATA_FILE_TEST)
     comments = df.Cleaned_Text.values
     y = df.human_label.values
-    
+
     return comments, y
+
 
 def base_model(X):
     '''Returns all 1s'''
     return np.ones(X.shape[0])
 
+
 def feature_importance(model, model_name, X_test, y_test, feature_names, output_dir):
-    
     if model_name == 'RandomForestClassifier':
-#        result = permutation_importance(model, X_test.toarray(), y_test, n_repeats=10, random_state=42, n_jobs=2)
-#        sorted_idx = result.importances_mean.argsort()
-#        
-#        fig, ax = plt.subplots()
-#        ax.boxplot(result.importances[sorted_idx].T,
-#                   vert=False, labels=X_test.columns[sorted_idx])
-#        ax.set_title("Permutation Importances (test set)")
-#        fig.tight_layout()
-#        plt.show()
-        
+        #        result = permutation_importance(model, X_test.toarray(), y_test, n_repeats=10, random_state=42, n_jobs=2)
+        #        sorted_idx = result.importances_mean.argsort()
+        #
+        #        fig, ax = plt.subplots()
+        #        ax.boxplot(result.importances[sorted_idx].T,
+        #                   vert=False, labels=X_test.columns[sorted_idx])
+        #        ax.set_title("Permutation Importances (test set)")
+        #        fig.tight_layout()
+        #        plt.show()
+
         tree_feature_importances = model.best_estimator_.feature_importances_
         indices = tree_feature_importances.argsort()[::-1]
         fig, ax = plt.subplots()
@@ -108,23 +121,24 @@ def feature_importance(model, model_name, X_test, y_test, feature_names, output_
         ax.set_title("Random Forest Feature Importances (MDI)")
         fig.tight_layout()
         plt.show()
-    
+
     if model_name == 'LogisticRegression':
         coefs = model.best_estimator_.coef_[0]
         indices = np.argsort(coefs)[::-1]
         fig, ax = plt.subplots()
-        
+
         names = feature_names[indices[:15]]
         y_ticks = np.arange(0, len(names))
         ax.barh(y_ticks, coefs[indices[:15]], color="r", align="center")
         ax.set_yticklabels(names)
         ax.set_yticks(y_ticks)
-        
+
         ax.set_title("Feature importances (Logistic Regression)")
         fig.tight_layout()
         plt.show()
-    
-    fig.savefig(os.path.join(output_dir, model_name+'_feat_imp.png'), dpi = 300)
+
+    fig.savefig(os.path.join(output_dir, model_name + '_feat_imp.png'), dpi=300)
+
 
 """
 Get performance score and confidence interval using bootstrapping.
@@ -137,38 +151,42 @@ Inputs:
 Returns:
     metric, lower limit of 95% CI, upper limit of 95% CI
 """
-def get_score(model_pred, y, score_function):    
+
+
+def get_score(model_pred, y, score_function):
     scores = []
 
     for i in range(2000):
-        #Bootstrap and calculate F1 score
+        # Bootstrap and calculate F1 score
         bootstrapped_sample = resample(y, model_pred)
         pred = bootstrapped_sample[0]
         lab = bootstrapped_sample[1]
         score = getattr(sklearn.metrics, score_function)(lab, pred)
         scores.append(score)
-      
+
     # confidence intervals
     alpha = 0.95
-    p = ((1.0-alpha)/2.0) * 100
+    p = ((1.0 - alpha) / 2.0) * 100
     lower = max(0.0, np.percentile(scores, p))
-    p = (alpha+((1.0-alpha)/2.0)) * 100
+    p = (alpha + ((1.0 - alpha) / 2.0)) * 100
     upper = min(1.0, np.percentile(scores, p))
-    
+
     # Mean
     mean = np.mean(scores)
-    
+
     return mean, lower, upper
 
-def print_CI(name, val, lower, upper):    
-    print("%-16s %.3f%% (%.3f%%, %.3f%%)" % (name+":", val * 100, lower * 100, upper * 100))
+
+def print_CI(name, val, lower, upper):
+    print("%-16s %.3f%% (%.3f%%, %.3f%%)" % (name + ":", val * 100, lower * 100, upper * 100))
+
 
 def run_classifier(X_train, y_train, X_test, y_test, feature_names, output_dir):
     for model_name, model in models.items():
-        print("**"*10, model_name, "**"*10)
-        
+        print("**" * 10, model_name, "**" * 10)
+
         # Fit and predict
-        final_model = GridSearchCV(model, model_params[model_name],scoring=scorer, n_jobs= -1, cv = 5, verbose= 0)
+        final_model = GridSearchCV(model, model_params[model_name], scoring=scorer, n_jobs=-1, cv=5, verbose=0)
         final_model.fit(X_train, y_train)
 
         pred_train = final_model.predict(X_train)
@@ -182,63 +200,93 @@ def run_classifier(X_train, y_train, X_test, y_test, feature_names, output_dir):
         print(final_model.best_score_)
         print('BEST_PARAMS')
         print(final_model.best_params_)
-        
+
         # Performance scores
         for evaluate in ['Base', 'Train', 'Test']:
-            if evaluate=='Base':
+            if evaluate == 'Base':
                 y = y_test
                 pred = pred_base
-            if evaluate=='Train':
+            if evaluate == 'Train':
                 y = y_train
                 pred = pred_train
-            if evaluate=='Test':
+            if evaluate == 'Test':
                 y = y_test
                 pred = pred_test
-            
+
             f1, f1_lower, f1_upper = get_score(y, pred, 'f1_score')
             recall, recall_lower, recall_upper = get_score(y, pred, 'recall_score')
             precision, precision_lower, precision_upper = get_score(y, pred, 'precision_score')
             acc, acc_lower, acc_upper = get_score(y, pred, 'accuracy_score')
-            
-            print_CI(evaluate+' f1', f1, f1_lower, f1_upper)
-            print_CI(evaluate+' recall', recall, recall_lower, recall_upper)
-            print_CI(evaluate+' precision', precision, precision_lower, precision_upper)
-            print_CI(evaluate+' accuracy', acc, acc_lower, acc_upper)
-        
+
+            print_CI(evaluate + ' f1', f1, f1_lower, f1_upper)
+            print_CI(evaluate + ' recall', recall, recall_lower, recall_upper)
+            print_CI(evaluate + ' precision', precision, precision_lower, precision_upper)
+            print_CI(evaluate + ' accuracy', acc, acc_lower, acc_upper)
+
         # Feature importance
         feature_importance(final_model, model_name, X_test, y_test, feature_names, output_dir)
-        
+
         # PR curves
         avg_precision_test = average_precision_score(y_test, pred_test)
-        
+
         disp = plot_precision_recall_curve(final_model, X_test, y_test)
-        disp.ax_.set_title('Precision-Recall curve: '
-                   'AP={0:0.2f}'.format(avg_precision_test), name = model_name)
-        disp.ax_.get_legend().remove() #NB: remove legend and add space to plot title
-        disp.figure_.savefig(os.path.join(output_dir, model_name+'_PR.png'), dpi = 300)
+        disp.ax_.set_title('{0} Precision-Recall curve: '
+                           'Avg. Pre.={1:0.2f}'.format(model_name, avg_precision_test), name=model_name)
+        disp.ax_.get_legend().remove()  # NB: remove legend and add space to plot title
+        disp.figure_.savefig(os.path.join(output_dir, model_name + '_PR.png'), dpi=300)
 
-def main(output_dir):
-    comments_train, y_train = load_train()
-    comments_test, y_test = load_test()
-    
-    tfidf = TfidfVectorizer(list(comments_train), strip_accents='unicode', decode_error= 'strict',
-                        stop_words = 'english',
-                       ngram_range=(1,1))
+def get_features(x):
+    tfidf = TfidfVectorizer(list(x), strip_accents='unicode', decode_error='strict',
+                            stop_words='english', min_df=2, max_df=0.8,
+                            ngram_range=(1, 2))
+    x_features = tfidf.fit_transform(x)
+    feature_names = np.array(tfidf.get_feature_names())  # NB
+    return feature_names, x_features
 
-    X_train = tfidf.fit_transform(comments_train)
-    X_test = tfidf.transform(comments_test)
-    feature_names = np.array(tfidf.get_feature_names()) # NB
-    
-    # Run classifier using snorkel pipeline
-    print("Training classifier using Snorkel pipeline ...")
-    run_classifier(X_train, y_train, X_test, y_test, feature_names, output_dir)
-    
+def main(output_dir, experiment='all'):
+    assert experiment in [1, 2, 'all'], print('experiment should be either 1, 2 or "all"')
+    if experiment == 1 or experiment == 'all':
+        comments_train, y_train = load_train()
+        comments_test, y_test = load_test()
+
+        X = np.concatenate((comments_train, comments_test))
+        feature_names, X_tfidf = get_features(X)
+        X_train = X_tfidf[:len(comments_train), :]
+        X_test = X_tfidf[len(comments_train):, :]
+
+        # Run classifier using snorkel pipeline
+        print("Training classifier using Snorkel pipeline ...")
+        run_classifier(X_train, y_train, X_test, y_test, feature_names, output_dir)
+
+    if experiment == 2 or experiment == 'all':
+        processed_data = '../data/data_processed/data_processed.csv'
+        label_dir = '../data/human_labels/'
+
+        df = pd.read_csv(processed_data)
+        human_labels = read_human_labels(label_dir)
+        df = df.merge(human_labels, indicator=True, how='left', on=['record_number'])
+        df = df.query('_merge=="both"').drop('_merge', axis=1)
+        X = df.Cleaned_Text.values
+        y = df.human_label.values
+
+        feature_names, X_tfidf = get_features(X)
+
+        kfold = StratifiedKFold(n_splits=5, random_state=random_state, shuffle=True)
+        for train_idx, test_idx in kfold.split(X_tfidf, y):
+            X_train = X_tfidf[train_idx, :]
+            X_test = X_tfidf[test_idx, :]
+            y_train = y[train_idx]
+            y_test = y[test_idx]
+            print("Training classifier using Human labels ...")
+            run_classifier(X_train, y_train, X_test, y_test, feature_names, output_dir)
+
     # TODO: Run CV experiment using human labels for training
-        
+
+
 if __name__ == "__main__":
     output_dir = '../data/classifier_output/'
-    
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    
-    main(output_dir)
+
+    main(output_dir, experiment=2)
---
+++ data_cleaning.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+
+search_words = {  # NB 3/17 - updated categories and key words
+    'diagnoses': ['suspect', 'diagnosis', 'diagnose', 'problem list', 'problem'],
+    'diagnostic_test': ['test', 'diagnostic', 'procedure'],
+    'physical_exam': ['physical'],
+    'history': ['history', 'concern'],
+    'treatment': ['treatment', 'summary', 'to go home', 'comment', 'recommendation', 'instructions', 'plan', 'follow',
+                  'medication']
+}
+
+# Lines needs to be removed from the text
+removed_strings = [
+    '[x if abnormal]',
+    'please continue the same medication regimine for her future doctor visits to the hospital.',
+    '<b>please contact us through phone 608-263-7600, or email at <b>primarycare@vetmed.wisc.edu</b> for any updates, questions, or concerns.</b>',
+    'remember to either: <ul> <li>click the add rx button to insert medications dispensed today or <li>click the cpy comment button to insert the \
+    current medication form for on-going medications</li></ul></font><!--  --></td></tr></tbody></table>',
+    '[expand on above diagnoses as needed. do not simply reiterate. may include: summary of client discussion, differentials, and patient care.\
+    *include alternative/future options for testing or treatment based on patient response.]',
+    '<b>if you have a local veterinarian who referred you to uw veterinary care, we will send him/her a report for this visit.</b>',
+    'please contact us via phone'
+]
+
+
+def process_data(train_path, output_path):
+    # reading raw data
+    data = pd.read_csv(train_path)
+    data.species = data.species.str.strip()
+
+    cols = data.columns
+    cols = cols.insert(0, 'record_number')
+    data['record_number'] = data.index
+    data = data.loc[:, cols]
+
+    data_canine = data.loc[data.species == 'Canine']
+    new_cols = ["Cleaned_Text", "History", "Physical_Exam", "Diagnoses", "Diagnostic_Test",
+                "Treatment"]  # NB 3/17 - update categories
+    data_canine[new_cols] = data_canine.apply(lambda x: process_row(x.comment_text),
+                                              axis=1, result_type="expand")
+    data_canine.to_csv(output_path, index=False)
+
+
+"""
+NB 3/17 - added to limit character length of titles
+"""
+
+
+def filter_titles(titles):
+    char_limit = 75
+
+    for title in list(titles):
+        keep = True
+        if len(title) > char_limit:
+            keep = False
+        if keep == False:
+            titles.remove(title)
+
+
+def find_features_row(cleaned_text, titles, keywords):
+    filter_titles(titles)  # NB 3/17
+
+    lines = cleaned_text.split("\n")
+    result_text = []
+    for i, l in enumerate(lines):
+        for keyword in keywords:
+            if l.find(keyword) > -1 and (l.strip().lower() in titles):  # NB 3/17 - needed for match
+                next_lines = ''
+                if i + 1 < len(lines):
+                    for j in range(1, len(lines) - i):
+                        next_lines = lines[i + j]
+                        if next_lines.strip().lower() in titles:  # NB 3/17 - needed for match
+                            break
+                        else:
+                            result_text.append(next_lines)
+
+    # NB 3/17 - Get unique lines in original order
+    indexes = np.unique(result_text, return_index=True)[1]
+    result_text = [result_text[index] for index in sorted(indexes)]
+
+    return " ".join(result_text)  # NB 3/17 - space to prevent words from being concatenated
+
+
+def process_row(comment_html_o):
+    if comment_html_o is np.nan:
+        return ('', '', '', '', '', '')
+
+    # if verbose: print(comment_html)
+    comment_html = comment_html_o.lower()
+    comment_html = comment_html.replace("<b>", "<b>\n")
+    comment_html = comment_html.replace("</b>", "\n</b>")
+    comment_html = comment_html.replace("dr.", "dr")
+    comment_html = re.sub('<div>\[\s\]\s.*?</div>', '', comment_html)
+
+    for r_string in removed_strings:
+        comment_html = comment_html.replace(r_string, '')
+        # remove the entire line
+        # TODO
+
+    soup = BeautifulSoup(comment_html)
+
+    titles = soup.findAll('b')
+    titles = [t.text.strip().lower() for t in titles]
+
+    soup_text = soup.text
+    cleaned_text = re.sub('\[.*?\]', '', soup_text)
+    cleaned_text = re.sub('\\xa0', '', cleaned_text)
+    cleaned_text = re.sub('[ ]{2,}', '\n', cleaned_text)
+
+    # removing all instruructive sentences like  [instruction]
+    cleaned_text = re.sub('\[[a-z\s\d\-,:\(\)!;\.\"\\/#$\*%]{2,}\]', '\n', cleaned_text)
+    # removing unchecked sentences
+    cleaned_text = re.sub('\[\s\].*', '', cleaned_text)
+    # remove [x] marks
+    cleaned_text = re.sub('\[x\]', '', cleaned_text)
+
+    # remove dates
+    cleaned_text = re.sub('[0-9]{1,2}[\/,:][0-9]{1,2}[\/,:][0-9]{2,4}', '[DATE]', cleaned_text)
+
+    # remove special characters
+    cleaned_text = re.sub('[:,;%&*()$#-]', '', cleaned_text)
+
+    # remove numbers
+    cleaned_text = re.sub('\s[0-9\.].*?(\s|\n)', ' [NUM] ', cleaned_text)
+
+    # remove remaining numbers
+    cleaned_text = re.sub('\s[0-9\.].*?(\s|\n)', ' [NUM] ', cleaned_text)
+
+    # remove newline characters
+    cleaned_text = re.sub('\\n', ' ', cleaned_text)
+    # remove optionals am/pm like text
+    cleaned_text = re.sub('[a-z]{1,}\/[a-z]{1,}', ' ', cleaned_text)
+    # remove numbers and dates
+    cleaned_text = re.sub('\[date\]|\[num\]', ' ', cleaned_text)
+    # remove emails
+    cleaned_text = re.sub('[a-z1-9]{1,}@[a-z]{1,}\.(com|edu|uk|co)', ' ', cleaned_text)
+    # remove non a-z characters
+    cleaned_text = re.sub('[^a-z\s]', ' ', cleaned_text)
+    # removing all 1 or 2 char words
+    cleaned_text = re.sub('\s[a-z]{1,2}\s', ' ', cleaned_text)
+    cleaned_text = re.sub('\s[a-z]{1,2}\s', ' ', cleaned_text)
+    # removing extract space between the words
+    cleaned_text = re.sub('\s+', ' ', cleaned_text)
+
+    cleaned_text = cleaned_text.lower()
+
+    history = find_features_row(cleaned_text, titles, search_words['history'])
+    physical_exam = find_features_row(cleaned_text, titles, search_words['physical_exam'])
+    diagnoses = find_features_row(cleaned_text, titles, search_words['diagnoses'])
+    diagnostic_test = find_features_row(cleaned_text, titles,
+                                        search_words['diagnostic_test'])  # NB 3/17 - redefined categories
+    treatment = find_features_row(cleaned_text, titles, search_words['treatment'])
+
+    return (cleaned_text, history, physical_exam, diagnoses, diagnostic_test, treatment)
+
+
+if __name__ == '__main__':
+    process_data('../data/data_raw/data.csv',
+                 '../data/data_processed/data_processed.csv')
